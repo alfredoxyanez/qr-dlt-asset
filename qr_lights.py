2763f26f6ca6cf264fbf9818ac2eae92693f10ad
Alfredo Yanez <fredyyanez39@yahoo.com>
2019-03-05 23:19:38 -0500

    added light show

--- qr_lights.py
+++ qr_lights.py
@@ -12,6 +12,9 @@ import time
 import json
 import os
 import web3
+import time
+import board
+import neopixel
 from web3.auto import w3
 from web3 import Web3, HTTPProvider
 from web3.middleware import geth_poa_middleware
@@ -68,6 +71,22 @@ asset_contract = w3.eth.contract(
     abi=sc_abi
 )
 
+## neopixel
+# Choose an open pin connected to the Data In of the NeoPixel strip, i.e. board.D18
+# NeoPixels must be connected to D10, D12, D18 or D21 to work.
+pixel_pin = board.D18
+
+# The number of NeoPixels
+num_pixels = 24
+
+# The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
+# For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
+ORDER = neopixel.RGB
+
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.2, auto_write=False,
+                           pixel_order=ORDER)
+
+
 
 def check_in(id, nonce, location):
     """Short summary.
@@ -95,6 +114,58 @@ def decode(im) :
     # Find barcodes and QR codes
     decodedObjects = pyzbar.decode(im)
     return decodedObjects
+## NeoPixel Functions
+def wheel(pos):
+    # Input a value 0 to 255 to get a color value.
+    # The colours are a transition r - g - b - back to r.
+    if pos < 0 or pos > 255:
+        r = g = b = 0
+    elif pos < 85:
+        r = int(pos * 3)
+        g = int(255 - pos*3)
+        b = 0
+    elif pos < 170:
+        pos -= 85
+        r = int(255 - pos*3)
+        g = 0
+        b = int(pos*3)
+    else:
+        pos -= 170
+        r = 0
+        g = int(pos*3)
+        b = int(255 - pos*3)
+    return (r, g, b) if ORDER == neopixel.RGB or ORDER == neopixel.GRB else (r, g, b, 0)
+
+
+def rainbow_cycle(wait):
+    for j in range(255):
+        for i in range(num_pixels):
+            pixel_index = (i * 256 // num_pixels) + j
+            pixels[i] = wheel(pixel_index & 255)
+        pixels.show()
+        time.sleep(wait)
+
+def white_cycle(wait):
+    brightness = (0,0,0)
+    for i in range(num_pixels):
+        pixels[i] = brightness
+    pixels.show()
+    for _ in range(4):
+        for i in range(num_pixels):
+            brightness = ((brightness[0]+63) %255, (brightness[1]+63) %255, (brightness[2]+63) %255)
+            pixels[i] = brightness
+        pixels.show()
+        time.sleep(wait)
+def scanned(wait):
+    pixels.fill((0, 0, 0))
+    time.sleep(wait)
+    pixels.fill((0, 255, 0))
+    time.sleep(wait)
+    pixels.fill((0, 0, 0))
+    time.sleep(1)
+
+
+
 
 
 font = cv2.FONT_HERSHEY_SIMPLEX
@@ -106,6 +177,7 @@ while(cap.isOpened()):
     im = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     decodedObjects = decode(im)
+    white_cycle(0.001)
 
     for decodedObject in decodedObjects:
         points = decodedObject.polygon
@@ -131,10 +203,13 @@ while(cap.isOpened()):
         my_json = decodedObject.data.decode('utf8').replace("'", '"')
         data = json.loads(my_json)
         if data["id"] not in addresses.keys():
+            scanned(.5)
             print('Type : ', decodedObject.type)
             addresses[data["id"]]= True
-            n = check_in(data["id"], nonce, LOCATION)
-            nonce = n
+            rainbow_cycle(.001)
+            # n = check_in(data["id"], nonce, LOCATION)
+            # nonce = n
+
 
         barCode = str(decodedObject.data)
         cv2.putText(frame, barCode, (x, y), font, 1, (0,255,255), 2, cv2.LINE_AA)
