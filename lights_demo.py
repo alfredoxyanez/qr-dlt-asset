0fcc6d6708d9211c3494df07557d6fbef5dada14
Alfredo Yanez <fredyyanez39@yahoo.com>
2019-03-08 09:48:08 -0500

    added new light demo file

---
+++ lights_demo.py
@@ -0,0 +1,32 @@
+import neopixel
+import time
+
+
+
+pixel_pin = board.D18
+
+# The number of NeoPixels
+num_pixels = 24
+
+# The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
+# For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
+ORDER = neopixel.GRB
+
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.2, auto_write=False,
+                           pixel_order=ORDER)
+def circle(delay, to_color, from_color= (0,0,0)):
+    n = num_pixels
+    for i in range(num_pixels):
+        for j in range(n):
+            if j == n-1:
+                pixels[j] = to_color
+            elif j >0:
+                pixels[j] = to_color
+                pixels[j-1] = from_color
+            else:
+                pixels[j] = to_color
+            pixels.show()
+            time.sleep(.5)
+        n = n-1
+    time.sleep(1)
+    pixels.fill(from_color)
