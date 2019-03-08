b033ef0652865748de4a75fe8406a1c2ef7f6967
Alfredo Yanez <fredyyanez39@yahoo.com>
2019-03-08 11:06:59 -0500

    added new logic for seen code

--- qr_lights.py
+++ qr_lights.py
@@ -153,7 +153,7 @@ while(cap.isOpened()):
         my_json = decodedObject.data.decode('utf8').replace("'", '"')
         data = json.loads(my_json)
         if data["id"] not in addresses.keys():
-            circle(.01,(255,0,0))
+            circle(.01,(0,255,0))
             print('Type : ', decodedObject.type)
             addresses[data["id"]]= n
             m_rainbow_cycle(.005,2)
