40d80831067b9191ffa7f4be48af5a1003430633
Alfredo Yanez <fredyyanez39@yahoo.com>
2019-03-11 14:22:23 -0400

    added gps_2 demo

--- gps_2.py
+++ gps_2.py
@@ -1,16 +1,16 @@
 import gps
 
-# Listen on port 2947 (gpsd) of localhost
-session = gps.gps("localhost", "2947")
-session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
 
-
-report = session.next()
-result = {}
-while True:
-    if report['class'] == 'TPV':
-        result['time'] = report.time
-        result['lat'] = report.lat
-        result['lon'] = report.lon
-        break
-return result
+def get_gps():
+    # Listen on port 2947 (gpsd) of localhost
+    session = gps.gps("localhost", "2947")
+    session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
+    report = session.next()
+    result = {}
+    while True:
+        if report['class'] == 'TPV':
+            result['time'] = report.time
+            result['lat'] = report.lat
+            result['lon'] = report.lon
+            break
+    return result
