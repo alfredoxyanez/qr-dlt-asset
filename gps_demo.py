2deff3183e6a21bdd6897770895ec2752192a637
Alfredo Yanez <fredyyanez39@yahoo.com>
2019-03-11 12:29:43 -0400

    gps module

--- gps_demo.py
+++ gps_demo.py
@@ -8,22 +8,22 @@ while 1:
     # a = altitude, d = date/time, m=mode,
     # o=postion/fix, s=status, y=satellites
 
-    print
-    print ' GPS reading'
-    print '----------------------------------------'
-    print 'latitude    ' , session.fix.latitude
-    print 'longitude   ' , session.fix.longitude
-    print 'time utc    ' , session.utc, session.fix.time
-    print 'altitude    ' , session.fix.altitude
-    print 'eph         ' , session.fix.eph
-    print 'epv         ' , session.fix.epv
-    print 'ept         ' , session.fix.ept
-    print 'speed       ' , session.fix.speed
-    print 'climb       ' , session.fix.climb
+    print()
+    print(' GPS reading')
+    print('----------------------------------------')
+    print('latitude    ' , session.fix.latitude)
+    print('longitude   ' , session.fix.longitude)
+    print('time utc    ' , session.utc, session.fix.time)
+    print('altitude    ' , session.fix.altitude)
+    print('eph         ' , session.fix.eph)
+    print('epv         ' , session.fix.epv)
+    print('ept         ' , session.fix.ept)
+    print('speed       ' , session.fix.speed)
+    print('climb       ' , session.fix.climb)
 
-    print
-    print ' Satellites (total of', len(session.satellites) , ' in view)'
+    print()
+    print(' Satellites (total of', len(session.satellites) , ' in view)')
     for i in session.satellites:
-        print '\t', i
+        print('\t', i)
 
     time.sleep(3)
