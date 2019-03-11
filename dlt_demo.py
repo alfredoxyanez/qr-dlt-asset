d8fb0f1104cf26cd5d647223da958d93f34e0cb7
Alfredo Yanez <fredyyanez39@yahoo.com>
2019-03-11 11:39:05 -0400

    added new demo files and helpers

---
+++ dlt_demo.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+import pyzbar.pyzbar as pyzbar
+import numpy as np
+import cv2
+import time
+import json
+import os
+import web3
+import time
+import board
+import neopixel
+from web3.auto import w3
+from dlt_helpers import *
+
+# get the webcam:
+cap = cv2.VideoCapture(0)
+
+cap.set(3,1024)
+cap.set(4,768)
+
+# Address that have been scanned
+addresses = {}
+
+## Neopixel Set Up
+# Choose an open pin connected to the Data In of the NeoPixel strip
+# NeoPixels must be connected to D10, D12, D18 or D21 to work.
+pixel_pin = board.D18
+# The number of NeoPixels
+num_pixels = 24
+# The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
+# For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
+ORDER = neopixel.GRB
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.2, auto_write=False,
+                           pixel_order=ORDER)
+
+def decode(im) :
+    # Find barcodes and QR codes
+    decodedObjects = pyzbar.decode(im)
+    return decodedObjects
+
+clear_lights(pixels,(0,0,0))
+
+#Start Camera Cycle
+n = 0
+while(cap.isOpened()):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    # Our operations on the frame come here
+    im = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    print(cap.get(5))
+    decodedObjects = decode(im)
+
+
+    for decodedObject in decodedObjects:
+        my_json = decodedObject.data.decode('utf8').replace("'", '"')
+        data = json.loads(my_json)
+        if data["id"] not in addresses.keys():
+            m_rainbow_cycle(pixels, .005,1)
+            addresses[data["id"]] = n
+            circle(pixels, .01,(0,255,0))
+        elif data["id"] in addresses.keys() :
+            print( n , addresses[data["id"]])
+            if n - addresses[data["id"]] > 3:
+                scanned(.25, 2)
+                clear_lights(pixels,(0,0,0))
+
+        n = n + 1
+    # Display the resulting frame
+    key = cv2.waitKey(1)
+    if key & 0xFF == ord('q'):
+        break
+    elif key & 0xFF == ord('s'): # wait for 's' key to save
+        cv2.imwrite('Capture.png', frame)
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
---
+++ dlt_helpers.py
@@ -0,0 +1,80 @@
+def clear_lights(pix, color=(0,0,0)):
+    pixels.fill(color)
+    pixels.show()
+def sample(l,addition):
+    l.append(addition)
+    print(l)
+## NeoPixel Functions
+def wheel(pos):
+    # Input a value 0 to 255 to get a color value.
+    # The colours are a transition r - g - b - back to r.
+    if pos < 0 or pos > 255:
+        r = g = b = 0
+    elif pos < 85:
+        r = int(pos * 3)
+        g = int(255 - pos*3)
+        b = 0
+    elif pos < 170:
+        pos -= 85
+        r = int(255 - pos*3)
+        g = 0
+        b = int(pos*3)
+    else:
+        pos -= 170
+        r = 0
+        g = int(pos*3)
+        b = int(255 - pos*3)
+    return (r, g, b) if ORDER == neopixel.RGB or ORDER == neopixel.GRB else (r, g, b, 0)
+
+
+def rainbow_cycle(pix, wait):
+    for j in range(255):
+        for i in range(num_pixels):
+            pixel_index = (i * 256 // num_pixels) + j
+            pix[i] = wheel(pixel_index & 255)
+        pix.show()
+        time.sleep(wait)
+
+def m_rainbow_cycle(pix, wait, times):
+    for _ in range(times):
+        rainbow_cycle(wait)
+    pix.fill((0,0,0))
+    pix.show()
+
+
+def scanning(pix, wait, times):
+    for _ in range(times):
+        pix.fill((0, 255, 0))
+        pix.show()
+        time.sleep(wait)
+        pix.fill((0, 0, 0))
+        pix.show()
+        time.sleep(wait)
+
+
+def scanned(pix, wait, times):
+    for _ in range(times):
+        pix.fill((255, 75, 0))
+        pix.show()
+        time.sleep(wait)
+        pix.fill((0, 0, 0))
+        pix.show()
+        time.sleep(wait)
+
+def circle(pix, delay, to_color, from_color= (0,0,0)):
+    n = num_pixels
+    for i in range(num_pixels):
+        for j in range(n):
+            if j == n-1:
+                pix[j] = to_color
+            elif j >0:
+                pix[j] = to_color
+                pix[j-1] = from_color
+            else:
+                pix[j] = to_color
+            pix.show()
+            time.sleep(delay)
+        n = n-1
+    time.sleep(.5)
+    pix.fill(from_color)
+    pix.show()
